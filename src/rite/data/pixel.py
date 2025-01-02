8ed7b19b9c891c63ceb16d8fb8d23f013fc16d2f
Lars van Vianen <lars@vianen.com>
2025-01-02 16:36:50 +0100

    Update pixel.py

--- src/rite/data/pixel.py
+++ src/rite/data/pixel.py
@@ -28,6 +28,7 @@ Todo:
 
 # Import | Local Modules
 from .colour import Colour
+
 # from .geometry.point import Point
 
 
@@ -54,18 +55,26 @@ class Pixel(object):
         self.oid = int(_oid)
         self.name = str(_name)
         self.location = _location
-        #self.size = int(3)
-        #self.shape = Rectangle...
+        # self.size = int(3)
+        # self.shape = Rectangle...
         self.value = _value
 
     def __str__(self):
         """"""
-        return "Pixel: Location({0} {1} {2}) Value({3} {4} {5} {6}) \n".format(self.location.x, self.location.y, self.location.z, self.value.r, self.value.g, self.value.b, self.value.a)
+        return "Pixel: Location({0} {1} {2}) Value({3} {4} {5} {6}) \n".format(
+            self.location.x,
+            self.location.y,
+            self.location.z,
+            self.value.r,
+            self.value.g,
+            self.value.b,
+            self.value.a,
+        )
 
     def position(self):
         """"""
-        return (self.location)
+        return self.location
 
     def value(self):
         """"""
-        return (self.value)
+        return self.value
