bf56ee22ce755b48f41b7f9b94ee1303278c010c
Lars van Vianen <lars@vianen.com>
2025-01-02 16:36:58 +0100

    Update colour.py

--- src/rite/data/colour.py
+++ src/rite/data/colour.py
@@ -29,10 +29,6 @@ Todo:
 # Import | Local Modules
 
 
-
-
-
-
 class Colour(object):
     """
     A class used to represent a Colour
@@ -50,14 +46,9 @@ class Colour(object):
 
     """
 
-
     # Magic Methods
 
-    def __init__(
-        self,
-        r,
-        g,
-        b):
+    def __init__(self, r, g, b):
         """Constructor of the object."""
         self._r = int(r)
         self._g = int(g)
@@ -66,16 +57,19 @@ class Colour(object):
 
     def __str__(self):
         """This method returns the string representation of the object."""
-        return "Color({0} {1} {2} {3})".format(self._r, self._g, self._b, self._a)
-
+        return "Color({0} {1} {2} {3})".format(
+            self._r, self._g, self._b, self._a
+        )
 
     # Static Methods
 
     @staticmethod
     def rgb_to_hex(rgb_tuple):
         """"""
-        #hex = '#%02x%02x%02x' % (rgb_tuple)
-        hex = "#{0:02x}{1:02x}{2:02x}".format(rgb_tuple[0], rgb_tuple[1], rgb_tuple[2])
+        # hex = '#%02x%02x%02x' % (rgb_tuple)
+        hex = "#{0:02x}{1:02x}{2:02x}".format(
+            rgb_tuple[0], rgb_tuple[1], rgb_tuple[2]
+        )
         return hex
 
     @staticmethod
@@ -90,7 +84,7 @@ class Colour(object):
         v = rgb_max
         if v == 0:
             h = s = 0
-            return (h,s,v)
+            return (h, s, v)
         # Compute the saturation value
         s = 255 * (rgb_max - rgb_min) // v
         if s == 0:
@@ -98,21 +92,21 @@ class Colour(object):
             return (h, s, v)
         # Compute the Hue
         if rgb_max == r:
-            h = 0 + 43*(g - b)//(rgb_max - rgb_min)
+            h = 0 + 43 * (g - b) // (rgb_max - rgb_min)
         elif rgb_max == g:
-            h = 85 + 43*(b - r)//(rgb_max - rgb_min)
-        else: # RGB_MAX == B
-            h = 171 + 43*(r - g)//(rgb_max - rgb_min)
+            h = 85 + 43 * (b - r) // (rgb_max - rgb_min)
+        else:  # RGB_MAX == B
+            h = 171 + 43 * (r - g) // (rgb_max - rgb_min)
         return (h, s, v)
 
     @staticmethod
     def hex_to_hsv(hex_value):
         """"""
         hex_value = hex_value.lstrip("#")
-        r, g, b = (int(hex_value[i:i+2], 16) / 255.0 for i in xrange(0,5,2))
+        r, g, b = (
+            int(hex_value[i : i + 2], 16) / 255.0 for i in xrange(0, 5, 2)
+        )
         return colorsys.rgb_to_hsv(r, g, b)
-
-
 
     def value(self):
         """"""
