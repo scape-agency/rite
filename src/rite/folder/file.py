16e817c9e6fdee168e6e9ded4cfa8b42defce97c
Lars van Vianen <lars@vianen.com>
2025-01-02 16:36:42 +0100

    Update file.py

--- src/rite/folder/file.py
+++ src/rite/folder/file.py
@@ -68,7 +68,7 @@ def copy_files(source_dir, target_dir):
     """
     """
     file_names = os.listdir(source_dir)
-        
+
     for file_name in file_names:
         # shutil.move(os.path.join(source_dir, file_name), target_dir)
         source = os.path.join(source_dir, file_name)
